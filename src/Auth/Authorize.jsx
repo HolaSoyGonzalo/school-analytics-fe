f6e9d90a166065c6dc46bd86ecd1970ba55fd1d0
Davide Pacco <72199035+HolaSoyGonzalo@users.noreply.github.com>
2021-04-12 20:04:56 +0200

    Heroku Sucks

--- src/Auth/Authorize.jsx
+++ src/Auth/Authorize.jsx
@@ -1,14 +1,15 @@
-import { useState, useEffect } from "react";
-
 const Authorize = async (setUser) => {
   try {
-    const response = await fetch("http://localhost:9999/home/user/me/", {
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + localStorage.getItem("accessToken"),
-      },
-    });
+    const response = await fetch(
+      "https://school-o-be.herokuapp.com/home/user/me/",
+      {
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("accessToken"),
+        },
+      }
+    );
     const data = await response.json();
     console.log(data);
     setUser(data);
